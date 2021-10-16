53df0764cd820cea44ae4f912c912ede53e82ca5
Le Thanh Viet <lethanhviet7c@gmail.com>
2021-10-17 01:59:16 +0700

    Setting UI

--- lib/src/screens/setting/setting.dart
+++ lib/src/screens/setting/setting.dart
@@ -1,4 +1,7 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:lettutor_mobile/src/screens/setting/setting_btn.dart';
 
 class SettingPage extends StatefulWidget {
   const SettingPage({Key? key}) : super(key: key);
@@ -10,6 +13,86 @@ class SettingPage extends StatefulWidget {
 class _SettingPageState extends State<SettingPage> {
   @override
   Widget build(BuildContext context) {
-    return const Text("Setting");
+    return Container(
+      margin: const EdgeInsets.only(top: 50, left: 30, right: 30),
+      child: Column(
+        children: [
+          Container(
+            margin: const EdgeInsets.only(bottom: 20),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Container(
+                  width: 70,
+                  height: 70,
+                  margin: const EdgeInsets.only(right: 20),
+                  decoration: const BoxDecoration(
+                    image: DecorationImage(image: AssetImage("asset/img/profile.jpg")),
+                    color: Colors.blue,
+                    borderRadius: BorderRadius.all(Radius.circular(1000)),
+                  ),
+                ),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Container(
+                      margin: const EdgeInsets.only(bottom: 5),
+                      child: const Text(
+                        "Le Thanh Viet",
+                        style: TextStyle(fontSize: 16, fontWeight: FontWeight.w500),
+                      ),
+                    ),
+                    const Text(
+                      "lethanhviet7c@gmail.com",
+                      style: TextStyle(color: Colors.grey),
+                    )
+                  ],
+                ),
+              ],
+            ),
+          ),
+          Column(
+            children: const <Widget>[
+              SettingButton(icon: "asset/svg/ic_user.svg", title: "View Feedbacks"),
+              SettingButton(icon: "asset/svg/ic_list.svg", title: "Booking History"),
+              SettingButton(icon: "asset/svg/ic_history.svg", title: "Session History"),
+              SettingButton(icon: "asset/svg/ic_setting2.svg", title: "Advanced Settings"),
+            ],
+          ),
+          Container(
+              margin: const EdgeInsets.only(top: 30),
+              child: Column(
+                children: const [
+                  SettingButton(icon: "asset/svg/ic_network.svg", title: "Our Website"),
+                  SettingButton(icon: "asset/svg/ic_facebook2.svg", title: "Facebook"),
+                ],
+              )),
+          Container(
+            margin: const EdgeInsets.only(top: 40),
+            child: ElevatedButton(
+              onPressed: () {},
+              child: Container(
+                padding: const EdgeInsets.only(top: 13, bottom: 13),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: const [
+                    Text(
+                      "Logout",
+                      style: TextStyle(color: Colors.white),
+                    )
+                  ],
+                ),
+              ),
+              style: ElevatedButton.styleFrom(
+                primary: Colors.blue,
+                shape: const RoundedRectangleBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(1000))),
+              ),
+            ),
+          )
+        ],
+      ),
+    );
   }
 }
---
+++ lib/src/screens/setting/setting_btn.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+class SettingButton extends StatelessWidget {
+  const SettingButton({Key? key, required String icon, required String title})
+      : _icon = icon,
+        _title = title,
+        super(key: key);
+
+  final String _icon, _title;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.only(bottom: 15),
+      child: ElevatedButton(
+        onPressed: () {},
+        child: Container(
+          padding: const EdgeInsets.only(top: 10, bottom: 10),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Row(
+                children: [
+                  SvgPicture.asset(
+                    _icon,
+                    width: 25,
+                    color: Colors.grey[700],
+                  ),
+                  Container(
+                    margin: const EdgeInsets.only(left: 15),
+                    child: Text(
+                      _title,
+                      style: TextStyle(color: Colors.grey[700], fontWeight: FontWeight.w400),
+                    ),
+                  )
+                ],
+              ),
+              SvgPicture.asset(
+                "asset/svg/ic_next.svg",
+                color: Colors.grey[700],
+              )
+            ],
+          ),
+        ),
+        style: ElevatedButton.styleFrom(
+          primary: Colors.white,
+          shape:
+              const RoundedRectangleBorder(borderRadius: BorderRadius.all(Radius.circular(1000))),
+        ),
+      ),
+    );
+  }
+}
