032ad9f2176ec9b3618cb455d1588c39320e619e
Le Thanh Viet <lethanhviet7c@gmail.com>
2021-11-20 02:12:20 +0700

    Search course, book view

--- lib/src/screens/courses_page/courses.dart
+++ lib/src/screens/courses_page/courses.dart
@@ -63,48 +63,169 @@ class _CoursesSearchPageState extends State<CoursesSearchPage> with TickerProvid
             Expanded(
               child: TabBarView(
                 children: [
-                  Container(
-                    margin: const EdgeInsets.only(bottom: 10, top: 10),
-                    child: TextField(
-                      style: TextStyle(fontSize: 12, color: Colors.grey[900]),
-                      decoration: InputDecoration(
-                          filled: true,
-                          fillColor: Colors.grey.shade200,
-                          prefixIcon: Container(
-                            padding: const EdgeInsets.all(13),
-                            child: SvgPicture.asset(
-                              "asset/svg/ic_search.svg",
-                              color: Colors.grey[600],
-                            ),
+                  SingleChildScrollView(
+                    child: Column(
+                      children: [
+                        Container(
+                          margin: const EdgeInsets.only(bottom: 10, top: 10),
+                          child: TextField(
+                            style: TextStyle(fontSize: 12, color: Colors.grey[900]),
+                            decoration: InputDecoration(
+                                filled: true,
+                                fillColor: Colors.grey.shade200,
+                                prefixIcon: Container(
+                                  padding: const EdgeInsets.all(13),
+                                  child: SvgPicture.asset(
+                                    "asset/svg/ic_search.svg",
+                                    color: Colors.grey[600],
+                                  ),
+                                ),
+                                contentPadding: const EdgeInsets.only(left: 5, right: 5),
+                                border: const OutlineInputBorder(
+                                  borderSide: BorderSide.none,
+                                  borderRadius: BorderRadius.all(
+                                    Radius.circular(10),
+                                  ),
+                                ),
+                                hintText: "Search courses..."),
                           ),
-                          contentPadding: const EdgeInsets.only(left: 5, right: 5),
-                          border: const OutlineInputBorder(
-                            borderSide: BorderSide.none,
-                            borderRadius: BorderRadius.all(
-                              Radius.circular(10),
-                            ),
-                          ),
-                          hintText: "Search courses..."),
-                    ),
-                  ),
-                  Container(
-                    margin: const EdgeInsets.only(bottom: 10, top: 10),
-                    child: TextField(
-                        style: TextStyle(fontSize: 12, color: Colors.grey[900]),
-                        decoration: InputDecoration(
-                            filled: true,
-                            fillColor: Colors.grey.shade200,
-                            prefixIcon: Container(
-                              padding: const EdgeInsets.all(13),
-                              child: SvgPicture.asset(
-                                "asset/svg/ic_search.svg",
-                                color: Colors.grey[600],
+                        ),
+                        InkWell(
+                          onTap: () {
+                            Navigator.pushNamed(context, "/course");
+                          },
+                          child: Card(
+                            elevation: 8,
+                            child: SizedBox(
+                              height: 300,
+                              child: Column(
+                                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                children: [
+                                  Image.asset(
+                                    "asset/img/course.png",
+                                    alignment: Alignment.center,
+                                    width: MediaQuery.of(context).size.width,
+                                    height: 210,
+                                    fit: BoxFit.cover,
+                                  ),
+                                  Container(
+                                    padding: const EdgeInsets.fromLTRB(10, 15, 10, 15),
+                                    child: Column(
+                                      crossAxisAlignment: CrossAxisAlignment.start,
+                                      children: [
+                                        const Text(
+                                          "Basic conversation Topics",
+                                          maxLines: 1,
+                                          overflow: TextOverflow.ellipsis,
+                                          style: TextStyle(fontSize: 17, fontWeight: FontWeight.bold),
+                                        ),
+                                        Container(
+                                          margin: const EdgeInsets.only(top: 8),
+                                          child: Row(
+                                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                            children: [
+                                              Text(
+                                                "Beginer",
+                                                style: TextStyle(fontSize: 15, color: Colors.grey[800]),
+                                              ),
+                                              Text(
+                                                "10 Lessons",
+                                                style: TextStyle(fontSize: 12, color: Colors.grey[800]),
+                                              ),
+                                            ],
+                                          ),
+                                        )
+                                      ],
+                                    ),
+                                  )
+                                ],
                               ),
                             ),
-                            contentPadding: const EdgeInsets.only(left: 5, right: 5),
-                            border: const OutlineInputBorder(
-                                borderSide: BorderSide.none, borderRadius: BorderRadius.all(Radius.circular(10))),
-                            hintText: "Search books...")),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  SingleChildScrollView(
+                    child: Column(
+                      children: [
+                        Container(
+                          margin: const EdgeInsets.only(bottom: 10, top: 10),
+                          child: TextField(
+                            style: TextStyle(fontSize: 12, color: Colors.grey[900]),
+                            decoration: InputDecoration(
+                                filled: true,
+                                fillColor: Colors.grey.shade200,
+                                prefixIcon: Container(
+                                  padding: const EdgeInsets.all(13),
+                                  child: SvgPicture.asset(
+                                    "asset/svg/ic_search.svg",
+                                    color: Colors.grey[600],
+                                  ),
+                                ),
+                                contentPadding: const EdgeInsets.only(left: 5, right: 5),
+                                border: const OutlineInputBorder(
+                                  borderSide: BorderSide.none,
+                                  borderRadius: BorderRadius.all(
+                                    Radius.circular(10),
+                                  ),
+                                ),
+                                hintText: "Search books..."),
+                          ),
+                        ),
+                        InkWell(
+                          onTap: () {
+                            Navigator.pushNamed(context, "/course");
+                          },
+                          child: Card(
+                            elevation: 8,
+                            child: SizedBox(
+                              height: 300,
+                              child: Column(
+                                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Image.asset(
+                                    "asset/img/book_cover.jpeg",
+                                    alignment: Alignment.center,
+                                    width: MediaQuery.of(context).size.width,
+                                    height: 170,
+                                    fit: BoxFit.cover,
+                                  ),
+                                  Container(
+                                    padding: const EdgeInsets.fromLTRB(10, 10, 10, 15),
+                                    child: Column(
+                                      crossAxisAlignment: CrossAxisAlignment.start,
+                                      children: [
+                                        const Text(
+                                          "Let's go starter",
+                                          maxLines: 1,
+                                          overflow: TextOverflow.ellipsis,
+                                          style: TextStyle(fontSize: 17, fontWeight: FontWeight.bold),
+                                        ),
+                                        Container(
+                                          margin: const EdgeInsets.only(top: 8, bottom: 15),
+                                          child: Text(
+                                            "For kids who start learning English the first time or speak a little English only.",
+                                            maxLines: 2,
+                                            overflow: TextOverflow.ellipsis,
+                                            style: TextStyle(fontSize: 12, color: Colors.grey[800]),
+                                          ),
+                                        ),
+                                        Text(
+                                          "Beginner",
+                                          style: TextStyle(fontSize: 15, color: Colors.grey[800]),
+                                        ),
+                                      ],
+                                    ),
+                                  )
+                                ],
+                              ),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
                   ),
                 ],
               ),
