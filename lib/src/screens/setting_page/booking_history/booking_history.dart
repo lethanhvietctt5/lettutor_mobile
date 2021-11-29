26e781f5273776a6779e6ee4dd79878c62bd4076
Le Thanh Viet <lethanhviet7c@gmail.com>
2021-11-29 17:10:09 +0700

    Show empty notification when empty booking and upcomming

--- lib/src/screens/setting_page/booking_history/booking_history.dart
+++ lib/src/screens/setting_page/booking_history/booking_history.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:lettutor_mobile/src/models/user/booking.dart';
 import 'package:lettutor_mobile/src/provider/user_provider.dart';
 import 'package:lettutor_mobile/src/screens/setting_page/booking_history/booking_item.dart';
@@ -30,17 +31,39 @@ class BookingHistoryPage extends StatelessWidget {
             ),
           ),
         ),
-        body: Container(
-          margin: const EdgeInsets.only(left: 15, right: 15),
-          child: ListView.builder(
-            itemCount: bookingHistory.length,
-            itemBuilder: (context, index) => BookingItem(
-              idTutor: bookingHistory[index].tutor.id,
-              start: bookingHistory[index].start,
-              end: bookingHistory[index].end,
-            ),
-          ),
-        ),
+        body: bookingHistory.isNotEmpty
+            ? Container(
+                margin: const EdgeInsets.only(left: 15, right: 15),
+                child: ListView.builder(
+                  itemCount: bookingHistory.length,
+                  itemBuilder: (context, index) => BookingItem(
+                    idTutor: bookingHistory[index].tutor.id,
+                    start: bookingHistory[index].start,
+                    end: bookingHistory[index].end,
+                  ),
+                ),
+              )
+            : SizedBox(
+                height: MediaQuery.of(context).size.height * 0.8,
+                child: Center(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      SvgPicture.asset(
+                        "asset/svg/ic_empty.svg",
+                        width: 200,
+                      ),
+                      Container(
+                        margin: const EdgeInsets.only(top: 20),
+                        child: Text(
+                          "You don't have any booking...",
+                          style: TextStyle(color: Colors.grey[700]),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
       ),
     );
   }
--- lib/src/screens/upcoming_page/upcoming.dart
+++ lib/src/screens/upcoming_page/upcoming.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:lettutor_mobile/src/provider/user_provider.dart';
 import 'package:lettutor_mobile/src/screens/upcoming_page/card_coming.dart';
 import 'package:provider/provider.dart';
@@ -19,13 +20,36 @@ class _UpcomingPageState extends State<UpcomingPage> {
     return SingleChildScrollView(
       child: Container(
         margin: const EdgeInsets.fromLTRB(15, 10, 15, 10),
-        child: ListView.builder(
-            itemCount: userUpcomming.length,
-            itemBuilder: (context, index) {
-              return UpComingCard(upcomming: userUpcomming[index]);
-            },
-            shrinkWrap: true,
-            physics: const NeverScrollableScrollPhysics()),
+        child: userUpcomming.isNotEmpty
+            ? ListView.builder(
+                itemCount: userUpcomming.length,
+                itemBuilder: (context, index) {
+                  return UpComingCard(upcomming: userUpcomming[index]);
+                },
+                shrinkWrap: true,
+                physics: const NeverScrollableScrollPhysics(),
+              )
+            : SizedBox(
+                height: MediaQuery.of(context).size.height * 0.8,
+                child: Center(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      SvgPicture.asset(
+                        "asset/svg/ic_empty.svg",
+                        width: 200,
+                      ),
+                      Container(
+                        margin: const EdgeInsets.only(top: 20),
+                        child: Text(
+                          "You don't have any upcomming...",
+                          style: TextStyle(color: Colors.grey[700]),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
       ),
     );
   }
